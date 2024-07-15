431f9707c702f90a6f81aee4700fccaa61eccac4
Matheuspisil <ajudaserio@yahoo.com.br>
2024-07-15 18:53:48 -0300

    Feat: For deploy

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    if os.name == 'nt':  # Se estiver no Windows
+        from waitress import serve
+        serve(app, host='0.0.0.0', port=8080)
+    else:  # Se estiver no Unix/Linux
+        app.run(debug=True)
